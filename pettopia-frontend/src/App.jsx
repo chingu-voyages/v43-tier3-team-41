a2873036114b398d0ec61ebcc008b2b53325a198
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2023-03-15 21:36:10 -0400

    add formatting dependencies and tailwindcss/daisyui.

--- pettopia-frontend/src/App.jsx
+++ pettopia-frontend/src/App.jsx
@@ -1,18 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./HomePage/NavBar";
-import HomePage from "./HomePage/HomePage";
-import SearchPage from "./SearchPage/SearchPage";
-import ProductDetail from "./ProductPage/ProductDetail";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/navigation/NavBar';
+import HomePage from './pages/HomePage/HomePage';
+import SearchPage from './pages/SearchPage/SearchPage';
+import ProductDetail from './pages/ProductPage/ProductDetail';
 
 function App() {
-
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/product_for_now" element={<ProductDetail />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/search' element={<SearchPage />} />
+        <Route path='/product_for_now' element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
   );
