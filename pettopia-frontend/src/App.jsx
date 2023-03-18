3c82810155dc07cc9fa710a12db0e079a86beea3
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2023-03-18 17:55:19 -0400

    update navbar to be responsive and add theme change button

--- pettopia-frontend/src/App.jsx
+++ pettopia-frontend/src/App.jsx
@@ -8,12 +8,12 @@ function App() {
   return (
     <BrowserRouter>
       <NavBar />
-      <div className="container">
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/search' element={<SearchPage />} />
-        <Route path='/product_for_now' element={<ProductDetail />} />
-      </Routes>
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/search' element={<SearchPage />} />
+          <Route path='/product_for_now' element={<ProductDetail />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
--- pettopia-frontend/tailwind.config.js
+++ pettopia-frontend/tailwind.config.js
@@ -3,13 +3,13 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     container: {
-      center: true, 
-      padding: '1.25', 
+      center: true,
+      padding: '1.25',
       screens: {
         xl: '1200px',
-        '2xl': '1200px'
-      }
-    }
+        '2xl': '1200px',
+      },
+    },
   },
   plugins: [require('daisyui')],
 };
