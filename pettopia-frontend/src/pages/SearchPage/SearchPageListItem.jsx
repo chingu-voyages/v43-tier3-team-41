f287922b2d8719efae76910fa519a982fa446364
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2023-03-27 12:14:17 -0400

    refactor variable names to enhance readability of code.

--- pettopia-frontend/src/pages/SearchPage/SearchPageListItem.jsx
+++ pettopia-frontend/src/pages/SearchPage/SearchPageListItem.jsx
@@ -1,28 +1,33 @@
-const SearchPageListItem = ({item}) => {
-    
-    const brand = item.name.split(' ')
-    const meow = brand[0]
-    const rawr = brand.filter(item => item !== meow).join(' ')
-    console.log(meow)
-    console.log(rawr)
-
+const SearchPageListItem = ({ item }) => {
+  const splitName = item.name.split(' ');
+  const brand =
+    splitName[0] === 'Pet' ||
+    splitName[0] === 'The' ||
+    splitName[0] === 'Vibrant'
+      ? `${splitName[0]} ${splitName[1]}`
+      : splitName[0];
+  const description = splitName.filter((item) => item !== brand).join(' ');
 
   return (
     <li key={item.id}>
       <div className='card w-84 bg-base-100 shadow-xl h-84'>
         <figure>
-          <img className=""
-            src={item.mainImageUrl}
-            alt={item.name}
-          />
+          <img className='' src={item.mainImageUrl} alt={item.name} />
         </figure>
         <div className='card-body'>
           <h2 className='card-title'>
-            {`${meow}`}
+            {`${brand}`}
             <div className='badge badge-secondary'>{`$${item.price}`}</div>
           </h2>
-          <p className="card-paragraph"> {`${rawr.length > 50 ? rawr.slice(0, 50) + '...' : rawr}`} </p>
-          {/* <p>{item.description}</p> */}
+          <p className='card-paragraph'>
+            {' '}
+            {`${
+              description.length > 50
+                ? description.slice(0, 50) + '...'
+                : description
+            }`}{' '}
+          </p>
+          <p>{item.description}</p>
           <div className='card-actions justify-end'>
             <div className='badge badge-outline'>Fashion</div>
             <div className='badge badge-outline'>Products</div>
