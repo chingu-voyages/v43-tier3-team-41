9f60e64067b4666560b4fd36dcec132398cf3c3f
vijay <VijayIyer>
2023-03-29 18:22:13 -0400

    Changes related to add cart method in cart controller

--- pettopia-backend/src/controllers/cart.controller.js
+++ pettopia-backend/src/controllers/cart.controller.js
@@ -9,10 +9,16 @@ controller.createCart = (req, res) =>{
 controller.addToCart = (req, res) => {
 	console.log('calling add to cart!');
 	const { cartId } = req.params;
+	const productId = req.body.productId;
+	console.log(`${typeof productId}`);
 	let cart;
 	if (!cartId){
 			cart = new Cart({
-					items:[], 
+					items:[
+					{
+						productId: productId, 
+						quantity: 1
+					}], 
 					status:CART_ITEM_STATUS.Not_Processed
 				});
 			cart.save().then((cartObj) =>{
@@ -24,10 +30,33 @@ controller.addToCart = (req, res) => {
 			});
 		}
 	else{
-			res.status(200).json({
-			ok:true,
-			cartId:cartId
-		});
+		
+			const cart = Cart.find({ _id: cartId }).exec()
+			.then(async (cartObj) =>{
+				console.log(`here is the current cart - ${JSON.stringify(cartObj)}`)
+				let product = await Cart.find({_id:cartId}, {"items.productId": productId});
+				if(product){
+					product.quantity = product.quantity + 1;
+				}
+				else{
+					cartObj.items.push({
+						productId:productId, 
+						quantity: 1
+					})
+				}
+				
+					res.status(200).json({
+						ok:true, 
+						cartId: cartObj._id
+					})
+			})
+			.catch((err) =>{
+				res.status(500).
+				json({
+					ok:false, 
+					message: `add to cart failed with error ${err}`
+				})
+			})
 	}
 }
 
