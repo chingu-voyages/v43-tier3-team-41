8d2aae3e91f1ec495d6ce8ad4e8a2233f630787b
vijay <VijayIyer>
2023-04-02 01:28:11 -0400

    added new API methods for cart - subtract, empty and adding quantity in existing addToCart method

--- pettopia-backend/src/routes/cart.routes.js
+++ pettopia-backend/src/routes/cart.routes.js
@@ -7,6 +7,8 @@ const { isAuth } = require("../middlewares/authentication");
 router.post("/add/:productId", cartCTRL.addToNewCart);
 router.post("/:id/add/:productId", cartCTRL.addToCart);
 router.post("/:id/remove/:productId", cartCTRL.removeProductFromCart);
+router.post("/:id/subtract/:productId", cartCTRL.subtract);
+router.post("/:id/empty", cartCTRL.emptyCart);
 router.get("/:cartId", cartCTRL.getCart);
 
 module.exports = router;
