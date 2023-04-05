2876fbc1da369b27ff9bf933c721df1d9aa59695
vijay <VijayIyer>
2023-04-05 07:02:23 -0400

    Add required components and routes for signup and login

--- pettopia-frontend/src/App.jsx
+++ pettopia-frontend/src/App.jsx
@@ -5,6 +5,8 @@ import SearchPage from './pages/SearchPage/MainContent/SearchPage';
 import ProductDetail from './pages/ProductPage/ProductDetail';
 import CartPage from './pages/CartPage';
 import AppContext from './AppContext';
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
 
 function App() {
   const cartId = '6424c6249193db4836879fe6';
@@ -19,6 +21,8 @@ function App() {
           <Route path='/' element={<HomePage />} />
           <Route path='/search' element={<SearchPage />} />
           <Route path='/cart' element={<CartPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/signup' element={<SignUpPage />} />
           <Route path='/product_for_now' element={<ProductDetail />} />
         </Routes>
         </div>
