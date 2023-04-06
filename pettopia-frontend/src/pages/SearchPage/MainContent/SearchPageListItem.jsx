3e09b47e70f9abbd4f1f192655d4f960b9246938
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2023-04-06 15:39:02 -0400

    refactor search card badges

--- pettopia-frontend/src/pages/SearchPage/MainContent/SearchPageListItem.jsx
+++ pettopia-frontend/src/pages/SearchPage/MainContent/SearchPageListItem.jsx
@@ -1,22 +1,23 @@
 const SearchPageListItem = ({ item }) => {
   const splitName = item.name.split(' ');
-  const brand =
+  const defaultBrand =
     splitName[0] === 'Pet' ||
     splitName[0] === 'The' ||
     splitName[0] === 'Vibrant'
       ? `${splitName[0]} ${splitName[1]}`
       : splitName[0];
-  const description = splitName.filter((item) => item !== brand).join(' ');
+  const description = splitName.filter((item) => item !== defaultBrand).join(' ');
+  
 
   return (
-    <li className="" key={item.id}>
+    <li key={item.id}>
       <div className='card w-[100%] bg-base-100 shadow-xl h-84'>
         <figure>
-          <img className='' src={item.mainImageUrl} alt={item.name} />
+          <img src={item.mainImageUrl} alt={item.name} />
         </figure>
         <div className='card-body min-h-[190px]'>
           <h2 className='card-title'>
-            {`${brand}`}
+            {`${item.brand ?? defaultBrand}`}
             <div className='badge badge-secondary'>{`$${item.price}`}</div>
           </h2>
           <p className='card-paragraph'>
@@ -29,12 +30,15 @@ const SearchPageListItem = ({ item }) => {
           </p>
           <p>{item.description}</p>
           <div className='card-actions justify-end'>
-            {item.categories.map((category, index) => {
+            <div className='badge badge-outline'>{item.categories[item.categories.length - 1] ?? 'Dogs'}</div>
+            
+            {/* Show all Categories as badges */}
+            {/* {item.categories.map((category, index) => {
                 return(
-                  <div className='badge badge-outline'>{category.length > 10 ? category.slice(0,15) + '...' : category}</div>
+                  <div key={index} className='badge badge-outline'>{category.length > 10 ? category.slice(0,15) + '...' : category}</div>
                 )
               })
-            }
+            } */}
           </div>
         </div>
       </div>
