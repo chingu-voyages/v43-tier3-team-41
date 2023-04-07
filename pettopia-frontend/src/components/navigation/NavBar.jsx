60d47b20fe085725b65519a71794b83e7fac63d1
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2023-04-06 20:57:55 -0400

    enhance ui design of navbar

--- pettopia-frontend/src/components/navigation/NavBar.jsx
+++ pettopia-frontend/src/components/navigation/NavBar.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useContext } from 'react';
+import {Link} from 'react-router-dom';
 import SearchContext from '../../Context/SearchContext/SearchContext';
 
 const THEMES = [
@@ -46,11 +47,11 @@ export default function NavBar() {
     setTheme(themeVal);
   };
 
-  const {handleSubmit, searchTerm, setSearchTerm} = useContext(SearchContext);
+  const { handleSubmit, searchTerm, setSearchTerm } = useContext(SearchContext);
 
   return (
-    <header className='w-100'>
-      <div className='navbar bg-base-100 container sticky z-10'>
+    <header className='w-100 bg-gray-600'>
+      <div className='navbar container sticky z-10'>
         <div className='navbar-start'>
           <div className='dropdown'>
             <label
@@ -92,94 +93,20 @@ export default function NavBar() {
                     <path d='M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z' />
                   </svg>
                 </a>
-                <ul className='p-2'>
-                  <li>
-                    <a href='/'>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a href='/'>Submenu 2</a>
-                  </li>
-                </ul>
               </li>
               <li>
-                <a href='/'>Item 3</a>
+                <Link to='/search' className="font-medium text-white">
+                  All Products
+                </Link>
               </li>
             </ul>
           </div>
-          <a href='/' className='btn btn-ghost normal-case text-xl px-1'>
+          <a href='/' className='btn btn-ghost normal-case text-xl px-1 text-white transition ease-in-out delay-80 hover:-translate-y-1 hover:scale-110 duration-300 ...'>
             Pettopia
           </a>
-        </div>
-        <div className='navbar-center hidden lg:flex'>
-          <ul className='menu menu-horizontal px-1'>
-            <li>
-              <a href='/'>Item 1</a>
-            </li>
-            <li tabIndex={0}>
-              <a href='/'>
-                Parent
-                <svg
-                  className='fill-current'
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='20'
-                  height='20'
-                  viewBox='0 0 24 24'
-                >
-                  <path d='M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z' />
-                </svg>
-              </a>
-              <ul className='p-2'>
-                <li>
-                  <a href='/'>Submenu 1</a>
-                </li>
-                <li>
-                  <a href='/'>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href='/'>Item 3</a>
-            </li>
-          </ul>
-        </div>
-        <form
-          onSubmit={(e) => {
-            handleSubmit(e);
-          }}
-          className='form-control flex-none gap-2'
-        >
-          <div className="relative flex items-center">
-          <input
-            type='text'
-            placeholder='Search'
-            className='input input-bordered'
-            value={searchTerm}
-            onChange={(e) => {
-              setSearchTerm(e.target.value);
-            }}
-          />
-          <button type='submit' className='btn btn-ghost btn-circle absolute right-0 text-gray-400'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-5 w-5'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
-              />
-            </svg>
-          </button>
-          </div>
-        </form>
-        <div className='navbar-end'>
-          <div className='dropdown dropdown-end'>
-            <label tabIndex={0} className='btn'>
-              Choose Page Theme
+          <div className='dropdown dropdown-end ml-3'>
+            <label tabIndex={0} className='btn btn-outline transition ease-in-out delay-80 hover:-translate-y-1 hover:scale-110 hover:bg-inherit hover:border-inherit duration-300 ... text-white'>
+              Theme
             </label>
             <ul
               tabIndex={0}
@@ -198,6 +125,90 @@ export default function NavBar() {
               ))}
             </ul>
           </div>
+        </div>
+        <div className='navbar-center hidden lg:flex'>
+          <ul className='menu menu-horizontal px-1'>
+            <li>
+                <Link to='/search' className="text-white">
+                  All Products
+                </Link>
+            </li>
+          </ul>
+        </div>
+        <form
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
+          className='form-control flex-none gap-2'
+        >
+          <div className='relative flex items-center'>
+            <input
+              type='text'
+              placeholder='Search'
+              className='input input-bordered'
+              value={searchTerm}
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+              }}
+            />
+            <button
+              type='submit'
+              className='btn btn-ghost btn-circle absolute right-0 text-gray-400'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-5 w-5'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+                />
+              </svg>
+            </button>
+          </div>
+        </form>
+        <div className='navbar-end'>
+        <div className='dropdown dropdown-end'>
+
+        
+
+          <label tabIndex={0} className='btn btn-ghost btn-circle'>
+            <div className='indicator'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-7 w-7'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='#F3EEEE'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
+                />
+              </svg>
+              <span className='badge badge-sm indicator-item'>8</span>
+            </div>
+          </label>
+          <div
+            tabIndex={0}
+            className='mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow'
+          >
+            <div className='card-body'>
+              <span className='font-bold text-lg'>8 Items</span>
+              <span className='text-info'>Subtotal: $999</span>
+              <div className='card-actions'>
+                <button className='btn btn-primary btn-block'>View cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
         </div>
       </div>
     </header>
