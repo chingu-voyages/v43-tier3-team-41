63183b2ccc49900b4e4cf83c0a5cde49c4c0e839
vijay <VijayIyer>
2023-04-11 01:42:14 -0400

    added redis cache deployed url

--- pettopia-backend/src/redisConfig.js
+++ pettopia-backend/src/redisConfig.js
@@ -2,7 +2,7 @@ const redis = require('redis');
 let redisClient;
 
 (async () => {
-  redisClient = redis.createClient({
+  redisClient = redis.createClient('redis://red-cgqeu3e4dad5es0flv3g:6379', {
   	legacyMode:true
   });
   console.log('running redis client');
